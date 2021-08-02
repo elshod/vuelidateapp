0e1768d09ec213ef72534d1698bde129ff2a04ed
elshod <elshod5f@gmail.com>
2021-08-02 20:50:39 +0500

    'vuelidate'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+        <div>
+          <label class="form-label">Email</label>
+          <input type="email" class="form-control"
+            :class="{'is-invalid':$v.email.$error,'is-valid':!$v.email.$error && $v.email.$dirty}"
+            v-model='email'
+            @blur='$v.email.$touch()' >
+          <div class="invalid-feedback" v-show='!$v.email.required'>Iltimos maydonni to'ldiring</div>
+          <div class="invalid-feedback" v-show='!$v.email.email'>Maydonga email formatidagi ma'lumot kiriting</div>
+        </div>
+        <div>
+          <label class="form-label">Ism</label>
+          <input type="text"
+            class="form-control"
+            v-model='name'
+            @blur='$v.name.$touch()'
+            :class="{'is-invalid':$v.name.$error,'is-valid':!$v.name.$error && $v.name.$dirty}">
+          <div class="invalid-feedback" v-show='!$v.name.required'>Iltimos maydonni to'ldiring</div>
+          <div class="invalid-feedback" v-show='!$v.name.minLength'>Maydon eng kamida 5 ta simvol yozilsin</div>
+        </div>
+        <button class="mt-3 btn btn-primary" type="submit" :class="{'disabled':$v.$invalid}">Jo'natish</button>
+        <div class="d-none">{{$v}}</div>
+    </div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import {required, email, minLength} from 'vuelidate/lib/validators'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return { 
+      email:'', 
+      name:'' }
+  },
+  validations: {
+    email: {  
+      required, 
+      email 
+    },
+    name: {
+      required,
+      minLength: minLength(5)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
